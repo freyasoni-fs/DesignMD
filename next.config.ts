bb842b71f0c5d9b8570875f9742a9a2361ed98d6
freyasoni-fs <freya.soni@esparkbizmail.com>
2026-09-09 12:58:16 +0530

    Fix next config typing

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["playwright-core", "@sparticuz/chromium"],
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/**/*': [
-        './node_modules/playwright-core/**/*',
-        './node_modules/@sparticuz/chromium/**/*'
-      ],
-    },
+  outputFileTracingIncludes: {
+    '/api/**/*': [
+      './node_modules/playwright-core/**/*',
+      './node_modules/@sparticuz/chromium/**/*'
+    ],
   },
 };
 
