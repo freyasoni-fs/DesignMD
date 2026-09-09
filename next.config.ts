26456962a58ec1c182aac0c36f23d8ba65228fc5
freyasoni-fs <freya.soni@esparkbizmail.com>
2026-09-09 12:54:28 +0530

    Force include playwright files in Vercel bundle

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["playwright-core", "@sparticuz/chromium"],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': [
+        './node_modules/playwright-core/**/*',
+        './node_modules/@sparticuz/chromium/**/*'
+      ],
+    },
+  },
 };
 
 export default nextConfig;
